a9dc00481b7e50a2cf1823fb3717d59abdc7522b
Hizzex <adkipana@gmail.com>
2021-09-16 21:14:07 +0700

    Contoh1

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1>";
+		echo "Nama Saya Gatfan Handwiyantoro
+			Saya Tinggal Di Villa Mas Indah Block A";
+	}
+}
+ ?>
